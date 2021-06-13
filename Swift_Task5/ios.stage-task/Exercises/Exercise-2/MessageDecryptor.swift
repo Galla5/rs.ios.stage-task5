8573e2b9d32628ff1dc4cb3fde039d6346cb0878
Poliakowa Galina <galapoliakowa2015@gmail.com>
2021-06-13 16:02:16 +0300

    feat: add task2 part solution.

--- Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
@@ -3,6 +3,43 @@ import UIKit
 class MessageDecryptor: NSObject {
     
     func decryptMessage(_ message: String) -> String {
-        return ""
+        var resultString = ""
+        let query = "[0-9]{1,3}\\[[a-z]{1,}"
+
+        let ranges = message.ranges(of: query, options: .regularExpression)
+        let matches = ranges.map { message[$0] }
+//
+        for match in matches {
+            let result = match.split(separator: "[")
+            if result.count == 2 {
+                var repeatCount = Int(result[0]) ?? 1
+                if repeatCount == 0 {
+                    repeatCount = 1
+                }
+                for _ in 1...repeatCount {
+                    resultString.append(contentsOf: result[1])
+                }
+            }
+        }
+
+
+        
+        return resultString
+    }
+}
+
+//[0-9]{1,3}\[
+
+extension StringProtocol {
+    func ranges<S: StringProtocol>(of string: S, options: String.CompareOptions = []) -> [Range<Index>] {
+        var result: [Range<Index>] = []
+        var startIndex = self.startIndex
+        while startIndex < endIndex,
+            let range = self[startIndex...].range(of: string, options: options) {
+                result.append(range)
+                startIndex = range.lowerBound < range.upperBound ? range.upperBound :
+                    index(range.lowerBound, offsetBy: 1, limitedBy: endIndex) ?? endIndex
+        }
+        return result
     }
 }
