378ca02e27ff159d28fe9fda25f535a56aef906a
Poliakowa Galina <galapoliakowa2015@gmail.com>
2021-06-13 16:09:29 +0300

    feat: update task2 solution.

--- Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
@@ -3,18 +3,25 @@ import UIKit
 class MessageDecryptor: NSObject {
     
     func decryptMessage(_ message: String) -> String {
+        guard message.contains("[") else { return message }
+        let numbersRange = message.rangeOfCharacter(from: .decimalDigits)
+        guard numbersRange != nil else {
+            return message
+                .replacingOccurrences(of: "[", with: "")
+                .replacingOccurrences(of: "]", with: "")
+        }
         var resultString = ""
         let query = "[0-9]{1,3}\\[[a-z]{1,}"
 
         let ranges = message.ranges(of: query, options: .regularExpression)
         let matches = ranges.map { message[$0] }
-//
+
         for match in matches {
             let result = match.split(separator: "[")
             if result.count == 2 {
-                var repeatCount = Int(result[0]) ?? 1
+                let repeatCount = Int(result[0]) ?? 1
                 if repeatCount == 0 {
-                    repeatCount = 1
+                    break
                 }
                 for _ in 1...repeatCount {
                     resultString.append(contentsOf: result[1])
