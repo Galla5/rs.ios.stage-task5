78516c9d5e5c904795d569f2573574c3448644a5
Poliakowa Galina <galapoliakowa2015@gmail.com>
2021-06-13 15:12:46 +0300

    feat: add task1 part solution.

--- Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
@@ -1,8 +1,31 @@
 import Foundation
-
+//[8, 2, 3, 5, 4, 5]
 class StockMaximize {
 
     func countProfit(prices: [Int]) -> Int {
-        return -1
+        guard !prices.isEmpty else { return 0 }
+        guard Set(prices).count != 1 else { return 0 }
+        var profit = 0
+        let maxPrice = prices.max() ?? 0
+        let maxPriceIndex = prices.firstIndex(of: maxPrice) ?? 0
+        for (index, price) in prices.enumerated() {
+            if let nextDayPrice = prices[safe: index + 1] {
+                let nextDayProfit = nextDayPrice - price
+                var maxProfit = 0
+                if index < maxPriceIndex {
+                    maxProfit = maxPrice - price
+                }
+                let maxDayProfit = maxProfit > nextDayProfit ? maxProfit : nextDayProfit
+                profit += maxDayProfit
+            }
+        }
+        return profit
+    }
+}
+
+private extension Collection {
+
+    subscript (safe index: Index) -> Element? {
+        return indices.contains(index) ? self[index] : nil
     }
 }
