a885e6de3a28360e801a1d7f6f7a689cd39491fc
Poliakowa Galina <galapoliakowa2015@gmail.com>
2021-06-13 16:38:22 +0300

    feat: update task2 solution.

--- Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
@@ -11,22 +11,37 @@ class MessageDecryptor: NSObject {
                 .replacingOccurrences(of: "]", with: "")
         }
         var resultString = ""
-        let query = "[0-9]{1,3}\\[[a-z]{1,}"
+        let query = "[a-z]{0,}[0-9]{1,3}\\[[a-z]{1,}\\][a-z]{0,}"
 
         let ranges = message.ranges(of: query, options: .regularExpression)
         let matches = ranges.map { message[$0] }
+        
+        let text = "abc"
+        let index2 = text.index(text.startIndex, offsetBy: 2) //will call succ 2 times
+        let lastChar: Character = text[index2]
 
         for match in matches {
-            let result = match.split(separator: "[")
-            if result.count == 2 {
-                let repeatCount = Int(result[0]) ?? 1
-                if repeatCount == 0 {
-                    break
-                }
-                for _ in 1...repeatCount {
-                    resultString.append(contentsOf: result[1])
-                }
+            let openBreketsIndex = match.firstIndex(of: "[")
+            let prefix = match[..<openBreketsIndex!]
+            
+            let prefixWithoutDigit = prefix.components(separatedBy: CharacterSet.decimalDigits).joined()
+            resultString.append(contentsOf: prefixWithoutDigit)
+            
+            let closeBreketsIndex = match.firstIndex(of: "]")
+            
+            let repeatMessage = match[openBreketsIndex!...closeBreketsIndex!]
+                .replacingOccurrences(of: "[", with: "")
+                .replacingOccurrences(of: "]", with: "")
+            var repeatCount = Int(String(prefix).westernArabicNumeralsOnly) ?? 0
+            if repeatCount == 0 {
+                break
             }
+            for _ in 1...repeatCount {
+                resultString.append(contentsOf: repeatMessage)
+            }
+            
+            let postfix = match[closeBreketsIndex!...].replacingOccurrences(of: "]", with: "")
+            resultString.append(contentsOf: postfix)
         }
 
 
@@ -35,9 +50,7 @@ class MessageDecryptor: NSObject {
     }
 }
 
-//[0-9]{1,3}\[
-
-extension StringProtocol {
+private extension StringProtocol {
     func ranges<S: StringProtocol>(of string: S, options: String.CompareOptions = []) -> [Range<Index>] {
         var result: [Range<Index>] = []
         var startIndex = self.startIndex
@@ -48,5 +61,13 @@ extension StringProtocol {
                     index(range.lowerBound, offsetBy: 1, limitedBy: endIndex) ?? endIndex
         }
         return result
+    }
+}
+
+private extension String {
+    var westernArabicNumeralsOnly: String {
+        let pattern = UnicodeScalar("0")..."9"
+        return String(unicodeScalars
+            .flatMap { pattern ~= $0 ? Character($0) : nil })
     }
 }
